6f18112b2bfae1e85b23483ba50db60a145d9ec4
Thimira Vimukthi <ictthimira@gmail.com>
2023-07-06 10:10:25 +0530

    call the created component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-
+import PartOne from './components/PartOne';
 function App() {
   return (
     <div className="App">
       <h1>this is my first react project</h1>
-
+      <PartOne/>
     </div>
   );
 }
--- src/components/partOne.js
+++ src/components/partOne.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function partOne() {
   return (
     <div>
-      
+      <h1>This is my first react components</h1>
     </div>
   )
 }
